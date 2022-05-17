b7d2e1f4f3fd24068939dd4fceee07a75075cdf7
rinera182@gmail.com <rinera182@gmail.com>
2022-05-17 16:38:18 +0900

    new

--- Ex5_hint/Ex5_hint.cpp
+++ Ex5_hint/Ex5_hint.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
 using namespace std;
 
 /// <summary>
@@ -38,9 +40,72 @@ public:
 
 };
 
+class Cylinder :Solid {
+private:
+    double radius;
+    double height;
+public:
+    Cylinder(
+        double radius,   //底面の半径
+        double height) { //高さ
+        this->radius = radius;
+        this->height = height;
+    }
+    double GetVolume() {
+        return radius * radius * M_PI * height;
+    }
+    double GetSurface() {
+        return (radius * radius * M_PI * 2) + (radius * M_PI * height * 2);
+    }
+};
+
+class Cone :Solid {
+private:
+    double radius;
+    double height;
+public:
+    Cone(
+        double radius,   //底面の半径
+        double height) { //高さ
+        this->radius = radius;
+        this->height = height;
+    }
+    double GetVolume() {
+        return radius * radius * M_PI * height / 3;
+    }
+    double GetSurface() {
+        return radius * M_PI * sqrtf(radius * radius + height * height) + radius * radius * M_PI;
+    }
+};
+
+class Sphere :Solid {
+private:
+    double radius;
+public:
+    Sphere(
+        double radius) {  //球の半径
+        this->radius = radius;
+    }
+    double GetVolume() {
+        return radius * radius * radius * M_PI * 4 / 3;
+    }
+    double GetSurface() {
+        return radius * radius * M_PI * 4;
+    }
+};
+
 int main()
 {
     Box box{ 3,5,2.5 };
     cout << "boxの体積=" << box.GetVolume() << endl;
     cout << "boxの表面積=" << box.GetSurface() << endl;
+    Cylinder cylinder{ 5,12 };
+    cout << "cylinderの体積=" << cylinder.GetVolume() << endl;
+    cout << "cylinderの表面積=" << cylinder.GetSurface() << endl;
+    Cone cone{ 6,9 };
+    cout << "coneの体積=" << cone.GetVolume() << endl;
+    cout << "coneの表面積=" << cone.GetSurface() << endl;
+    Sphere sphere{ 7 };
+    cout << "sphereの体積=" << sphere.GetVolume() << endl;
+    cout << "sphereの表面積=" << sphere.GetSurface() << endl;
 }
